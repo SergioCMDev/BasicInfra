6cf5306f04b5153fb5050e7cc4c5266f2501a4bf
SergioCMDev <sergiocmdev@gmail.com>
2025-06-18 17:53:32 +0200

    Readded variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {More actions
+    type = string
+    description = "aws_region"
+    default = "eu-west-1"
+}
+
+variable "aws_availability_zones"{
+    type = list
+    description = "Availability zones"
+    default = ["eu-west-1a", "eu-west-1b"]
+}
+
+
+variable "ec2_ami"{
+    type = string
+    description = "AMI"
+    default = "ami-015b1e8e2a6899bdb" #Amazon Linux
+}
+
+variable "ec2_instance_type"{
+    type = string
+    description = "Instance type"
+    default = "t2.micro"
+}
+
+variable "public_ip" {
+  type = string
+  description = "self public ip"
+}
