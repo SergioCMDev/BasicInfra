0490268ce3657aee4c3e7ec8e336ad9b0227bd8b
SergioCMDev <sergiocmdev@gmail.com>
2025-06-19 19:25:04 +0200

    Added new ouptputs for BD

--- output.tf
+++ output.tf
@@ -5,10 +5,27 @@ output "vpc_id"{
 
 output "bd_id"{
     description = "ID BD"
-    value = aws_db_instance.rds_free_tier.id
+    value = aws_db_instance.wordpress_db.id
 }
 
 output "ec2_id"{
     description = "EC2 ID"
     value = aws_instance.ec2_instance.id
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.wordpress_db.endpoint
+}
+
+output "db_name" {
+  value = aws_db_instance.wordpress_db.db_name
+}
+
+output "db_username" {
+  value = aws_db_instance.wordpress_db.username
+}
+
+output "db_password" {
+  value = aws_db_instance.wordpress_db.password
+  sensitive = true
 }
