4929563c46d6f6c6e870413f216260de03bbd8de
SergioCMDev <sergiocmdev@gmail.com>
2025-06-19 19:23:06 +0200

    Fixed security group of EC2, Added User_data to install Wordpress

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,31 @@ resource "aws_instance" "ec2_instance" {
     ami = var.ec2_ami
     associate_public_ip_address = true
     instance_type = var.ec2_instance_type
-    vpc_security_group_ids = [aws_security_group.sg_base_ec2.id]
+    vpc_security_group_ids = [aws_security_group.sg_base_ec2.id,   aws_security_group.sg_front_end.id]
     subnet_id = aws_subnet.publicSubnetExample1.id
     key_name = aws_key_pair.kp_config_user.key_name
     tags = {
         Name = "Terraform_Instance"
     }
+
+    
+    user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y httpd php php-mysqlnd php-fpm php-opcache php-gd php-curl php-mbstring php-xml php-json php-dom
+
+              systemctl enable --now httpd
+              echo "¡Hello world!" > /var/www/html/index.html
+
+              cd /tmp
+              wget https://wordpress.org/latest.tar.gz
+              tar -xzvf latest.tar.gz
+
+              rm -f /var/www/html/index.html
+              cp -r wordpress/* /var/www/html/
+              chown -R apache:apache /var/www/html
+              chmod -R 755 /var/www/html
+
+              systemctl restart httpd
+              EOF
 }
