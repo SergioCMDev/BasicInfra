614e8d99adbc94413b5fa48945c168244991ed42
SergioCMDev <sergiocmdev@gmail.com>
2025-06-25 00:45:32 +0200

    removed sensitive values

--- output.tf
+++ output.tf
@@ -16,21 +16,3 @@ output "ec2_id" {
 output "db_endpoint" {
   value = aws_db_instance.wordpress_db.endpoint
 }
-
-output "db_name" {
-  value = aws_db_instance.wordpress_db.db_name
-}
-
-output "db_username" {
-  value = aws_db_instance.wordpress_db.username
-}
-
-output "db_password" {
-  value     = aws_db_instance.wordpress_db.password
-  sensitive = true
-}
-
-output "aws_iam_arn" {
-  value     = aws_iam_role.github_actions_role.arn
-  sensitive = true
-}
